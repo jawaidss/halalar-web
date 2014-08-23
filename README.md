31098fac1b15e686574bcd79943fd7a8d8993044
Samad Jawaid <samad@silviaterra.com>
2014-08-23 12:59:42 -0400

    Switch from SQLite to PostgreSQL

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 * [virtualenv](http://www.virtualenv.org/)
 * [virtualenvwrapper](http://virtualenvwrapper.readthedocs.org/)
 * [pip](http://www.pip-installer.org/)
+* [PostgreSQL](http://www.postgresql.org/)
 
 ## Installation
 
@@ -14,6 +15,10 @@ git clone git@github.com:jawaidss/halalar-web.git
 cd halalar-web
 mkvirtualenv halalar
 pip install -r requirements.txt
+sudo su postgres -c "createdb halalar"
+sudo su postgres -c "createuser -P halalar"
+sudo su postgres -c 'psql -c "ALTER USER halalar CREATEDB;"'
+sudo su postgres -c 'psql -c "GRANT ALL PRIVILEGES ON DATABASE halalar TO halalar;"'
 cd halalar
 python manage.py syncdb --noinput
 ```
--- halalar/halalar/settings.py
+++ halalar/halalar/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'vs=(w4f^h5bo+=#iiil4*&-@o%trp!-=21vxz$q^y(k2z5okzl')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'temp123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
@@ -64,8 +64,10 @@ WSGI_APPLICATION = 'halalar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'halalar',
+        'USER': 'halalar',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'temp123'),
     }
 }
 
